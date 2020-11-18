715cf63eb720f52f0f62e0ce5ce40d5369cd943c
Terence-hash <69487748+Terence-hash@users.noreply.github.com>
2020-11-18 17:29:26 +0800

    Update hustca-via.sh

--- hustca-via.sh
+++ hustca-via.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl --head --silent http://naihai.sinaapp.com | sed -E 's/Via: //g' | tail -2 | head -1 > hustca-via.txt
+curl --head --silent http://naihai.sinaapp.com | sed -E 's/Via: ad -1 > hustca-via.txt
